47c945f7118e6fbb3a620c71ebc0f593efb29906
Sebastian Sandqvist <seb@sparque.me>
2014-07-29 15:18:26 -0600

    controller setup for progress/quiz/translate/login

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,6 +3,26 @@ var controller = {
 		res.render('index', {
 			title: 'lingo'
 		})
+	},
+	progress: function(req, res) {
+		res.render('progress', {
+			title: 'progress'
+		})
+	},
+	quiz: function(req, res) {
+		res.render('quiz', {
+			title: 'quiz'
+		})
+	},
+	translate: function(req, res) {
+		res.render('translate', {
+			title: 'translate'
+		})
+	},
+	login: function(req, res) {
+		res.render('login', {
+			title: 'login'
+		})
 	}
 }
 

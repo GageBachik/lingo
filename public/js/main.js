607c2c14b2e6c13b9bc2859e030a57e7a2b145ee
Sebastian Sandqvist <seb@sparque.me>
2014-07-30 23:14:56 -0600

    using sass, made quiz template

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
-// fix fb auth weird URL with #_=_
+// temporary fix for fb auth weird URL with #_=_
+// should do this in the backend and actually fix the issue though
 // http://stackoverflow.com/a/18305085
 if (window.location.hash == '#_=_'){
     history.replaceState 
